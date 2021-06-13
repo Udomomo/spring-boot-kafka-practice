ce386da169a7427fc1d496fdfcba4416ec7003b3
Udomomo <23133164+Udomomo@users.noreply.github.com>
2021-06-13 11:21:16 +0900

    fix consumer to parse key and value

--- src/main/java/com/udomomo/springbootkafkaconsumer/KafkaConsumer.java
+++ src/main/java/com/udomomo/springbootkafkaconsumer/KafkaConsumer.java
@@ -1,12 +1,13 @@
 package com.udomomo.springbootkafkaconsumer;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaConsumer {
-  @KafkaListener(topics = "${kafka.topic}", groupId = "foo")
-  public void consume(String message) {
-    System.out.println(message);
+  @KafkaListener(topics = "${kafka.topic}")
+  public void consume(ConsumerRecord<String, String> record) {
+    System.out.println("key: " + record.key() + ", value: " + record.value());
   }
 }
--- src/main/java/com/udomomo/springbootkafkaconsumer/config/KafkaConfig.java
+++ src/main/java/com/udomomo/springbootkafkaconsumer/config/KafkaConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaStreamsConfiguration;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
